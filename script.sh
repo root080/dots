700d3b773cc13502e7657de487a4cc845adf15b2
Alex C <alexc@example.com>
2026-06-18 12:09:48 +0300

    Fixed install script

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 # Download background wallpaper
-curl https://w.wallhaven.cc/full/rr/wallhaven-rrl1kj.jpg --output /usr/share/wallpapers/wallhaven.jpg
+sudo curl https://w.wallhaven.cc/full/rr/wallhaven-rrl1kj.jpg --output /usr/share/wallpapers/wallhaven.jpg --create-dirs && sudo chown $(whoami) /usr/share/wallpapers/
 
 # Moving config files in place
-mv logid.cfg /etc/logid.cfg
-mv * .config/
+sudo mv logid.cfg /etc/logid.cfg
+mkdir -p ~/.config/ && mv * ~/.config/
